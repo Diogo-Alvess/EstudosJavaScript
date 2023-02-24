a34a81719df17463bb809e251ab750200643f787
Diogo <alves.diogoj@gmail.com>
2023-02-23 23:13:59 -0300

    Objts e prototypes

--- Avançado/index3.js
+++ Avançado/index3.js
@@ -81,15 +81,138 @@ console.log(p2.nome);
 */
 
 /*
+/*
+Javascript é baseado em protótipos para passar propriedades e métodos
+de um objeto para outro.
+Definição de protótipo
+Protótipo é o termo usado para se referir ao que foi criado pela
+primeira vez, servindo de modelo ou molde para futuras produções.
+Todos os objetos tem uma referência interna para um protótipo (__proto__)
+que vem da propriedade prototype da função construtora que foi usada para
+criá-lo. Quando tentamos acessar um membro de um objeto, primeiro o motor
+do JS vai tentar encontrar este membro no próprio objeto e depois a cadeia
+de protótipos é usada até o topo (null) até encontrar (ou não) tal membro.
+*/
+/*
+function Pessoa(nome, sobrenome) {
+  this.nome = nome;
+  this.sobrenome = sobrenome;
+  // this.nomeCompleto = () => 'ORIGINAL: ' + this.nome + ' ' + this.sobrenome;
+}
 
+// Pessoa.prototype === pessoa1.__proto__
+
+Pessoa.prototype.nomeCompleto = function() {
+  return this.nome + ' ' + this.sobrenome;
+};
+
+const pessoa1 = new Pessoa('Luiz', 'O.'); // <- Pessoa = Função construtora
+const pessoa2 = new Pessoa('Maria', 'A.'); // <- Pessoa = Função construtora
+const data = new Date(); // <-- Date = Função construtora
+
+console.dir(pessoa1);
+console.dir(data);
 */
 
 /*
+// POLIMOSFISMO
+// Superclass
+function Conta(agencia, conta, saldo) {
+  this.agencia = agencia;
+  this.conta = conta;
+  this.saldo = saldo;
+}
 
+Conta.prototype.sacar = function(valor) {
+  if(valor > this.saldo) {
+    console.log(`Saldo insuficiente: ${this.saldo}`);
+    return;
+  }
+
+  this.saldo -= valor;
+  this.verSaldo();
+};
+
+Conta.prototype.depositar = function(valor) {
+  this.saldo += valor;
+  this.verSaldo();
+};
+
+Conta.prototype.verSaldo = function() {
+  console.log(
+    `Ag/c: ${this.agencia}/${this.conta} | ` +
+    `Saldo: R$${this.saldo.toFixed(2)}`
+  );
+};
+
+function CC(agencia, conta, saldo, limite) {
+  Conta.call(this, agencia, conta, saldo);
+  this.limite = limite;
+}
+CC.prototype = Object.create(Conta.prototype);
+CC.prototype.constructor = CC;
+
+CC.prototype.sacar = function(valor) {
+  if(valor > (this.saldo + this.limite)) {
+    console.log(`Saldo insuficiente: ${this.saldo}`);
+    return;
+  }
+
+  this.saldo -= valor;
+  this.verSaldo();
+};
+
+function CP(agencia, conta, saldo) {
+  Conta.call(this, agencia, conta, saldo);
+}
+CP.prototype = Object.create(Conta.prototype);
+CP.prototype.constructor = CP;
+
+const cc = new CC(11, 22, 0, 100);
+cc.depositar(10);
+cc.sacar(110);
+cc.sacar(1);
+
+console.log();
+
+const cp = new CP(12, 33, 0);
+cp.depositar(10);
+cp.sacar(10);
+cp.sacar(1);
 */
 
 /*
+//Factory Functions + Prototypes
+const falar = {
+  falar() {
+    console.log(`${this.nome} está falando.`);
+  },
+};
 
+const comer = {
+  comer() {
+    console.log(`${this.nome} está comendo.`);
+  },
+};
+
+const beber = {
+  beber() {
+    console.log(`${this.nome} está bebendo.`);
+  },
+};
+
+const pessoaPrototype = Object.assign({}, falar, comer, beber);
+
+function criaPessoa(nome, sobrenome) {
+  return Object.create(pessoaPrototype, {
+    nome: { value: nome },
+    sobrenome: { value: sobrenome }
+  });
+}
+
+const p1 = criaPessoa('Luiz', 'Otávio');
+const p2 = criaPessoa('Maria', 'A.');
+console.log(p2);
 */
 
 /*
