a881a4cdd575eefc4d41f4773eb7702a7ef759ec
Diogo <alves.diogoj@gmail.com>
2023-03-01 19:16:58 -0300

    inicio OO

---
+++ Avançado/indexOO.js
@@ -0,0 +1,63 @@
+/*
+//Criando Classes
+
+class Pessoa {
+  constructor(nome, sobrenome) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+  }
+
+  falar() {
+    console.log(`${this.nome} está falando.`);
+  }
+}
+
+function Pessoa2(nome, sobrenome) {
+  this.nome = nome;
+  this.sobrenome = sobrenome;
+}
+
+Pessoa2.prototype.falar = function() {
+  console.log(`${this.nome} está falando.`);
+};
+
+const p1 = new Pessoa('Luiz', 'Miranda');
+const p2 = new Pessoa2('Luiz', 'Miranda');
+*/
+
+/*
+// GET E SET
+class Pessoa {
+  constructor(nome, sobrenome) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+  }
+
+  get nomeCompleto() {
+    return this.nome + ' ' + this.sobrenome;
+  }
+
+  set nomeCompleto(valor) {
+    valor = valor.split(' ');
+    this.nome = valor.shift();
+    this.sobrenome = valor.join(' ');
+  }
+}
+
+const p1 = new Pessoa('Luiz', 'Miranda');
+p1.nomeCompleto = 'Luiz Miranda Olibeira';
+console.log(p1.nome);
+console.log(p1.sobrenome);
+*/
+
+/*
+*/
+
+/*
+*/
+
+/*
+*/
+
+/*
+*/
